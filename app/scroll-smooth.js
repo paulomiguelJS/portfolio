5601988792adff716547630ca92df11dfe7c38de
paulomigueljs <paulomiguelblues@gmail.com>
2021-12-07 00:00:21 -0500

    Smooth scroll finished, link on logo

---
+++ app/scroll-smooth.js
@@ -0,0 +1,16 @@
+const interLinks = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'nearest',
+  });
+}
+
+interLinks.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
