4fea4a3d02f00c6ebd384a9de74e7a5848ba5005
paulomigueljs <paulomiguelblues@gmail.com>
2021-12-09 00:50:34 -0500

    GSAP added

--- app/elements-animation.js
+++ app/elements-animation.js
@@ -1,2 +1,29 @@
-const animeElement = document.querySelectorAll('#animeElement');
-console.log(animeElement);
+document.addEventListener('DOMContentLoaded', () => {
+  let tl = gsap.timeline();
+
+  tl.to('.menu-1', {
+    duration: 1,
+    x: 0,
+    opacity: 0,
+  })
+    .from('.menu-2', {
+      duration: 1,
+      opacity: 0,
+      delay: 1,
+    })
+    .from('.menu-3', {
+      duration: 1,
+      opacity: 0,
+      delay: 2,
+    })
+    .from('.menu-4', {
+      duration: 1,
+      opacity: 0,
+      delay: 2,
+    })
+    .from('.menu-5', {
+      duration: 1,
+      opacity: 0,
+      delay: 2,
+    });
+});
