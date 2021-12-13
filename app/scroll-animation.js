06ba8fe622835854a04a9ea9658c85f38fe2a19f
paulomigueljs <paulomiguelblues@gmail.com>
2021-12-13 15:03:23 -0500

    titles animation aborted

--- app/scroll-animation.js
+++ app/scroll-animation.js
@@ -1,15 +1,45 @@
-function animeScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  const halfWindow = window.innerHeight * 0.6;
+function initAnimeScroll() {
+  function animeScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    const halfWindow = window.innerHeight * 0.6;
 
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    const isSectionVisible = sectionTop - halfWindow < 0;
-    if (isSectionVisible) {
-      section.classList.add('active');
-    } else {
-      section.classList.remove('active');
-    }
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - halfWindow < 0;
+      if (isSectionVisible) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    });
+  }
+  window.addEventListener('scroll', animeScroll);
+}
+
+initAnimeScroll();
+
+/*
+function animeTitleRight() {
+  const animeTitles = document.querySelector('.text-right');
+
+  window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+    const val = scrolled * 0.2;
+    animeTitles.style.transform = `translateX(${-0.13 * val}%)`;
   });
 }
-window.addEventListener('scroll', animeScroll);
+
+animeTitleRight();
+
+function animeTitleLeft() {
+  const animeTitles = document.querySelector('.text-left');
+
+  window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+    const val = scrolled * 0.2;
+    animeTitles.style.transform = `translateX(${0.3 * val}%)`;
+  });
+}
+
+animeTitleLeft();
+*/
